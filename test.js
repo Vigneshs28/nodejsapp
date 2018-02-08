3192e363f9a02650b8d032c1d58fe7bb3d3325ab
tetradev01 <tetradev01@tetratutorials.com>
2018-02-08 23:26:01 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var request = require('request');
 
 it('Main page content', function(done) {
     request('http://127.0.0.1:1337/' , function(error, response, body) {
-        expect(body).to.equal('Hello World');
+        expect(body).to.equal('Welcome to TetraNoodle');
         done();
     });
 });
