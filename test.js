9803a51faa91f129f96ccdee89146730c1e1764b
tetradev01 <tetradev01@tetratutorials.com>
2018-02-08 23:05:13 +0530

    IP address updated in test

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var expect  = require('chai').expect;
 var request = require('request');
 
 it('Main page content', function(done) {
-    request('http://10.12.108.11:1337/' , function(error, response, body) {
+    request('http://127.0.0.1:1337/' , function(error, response, body) {
         expect(body).to.equal('Hello World');
         done();
     });
