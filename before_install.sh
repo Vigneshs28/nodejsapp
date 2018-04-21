1d922d8f9130b84f3991e7d0bfa5d299e3b96557
root <root@vagrant.vm>
2018-04-21 19:55:54 +0000

    updated appspec file.

--- before_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#sudo set -e
-set +o posix
-
-cd /home/
-sudo chmod -R 755 /home/ec2-user/
-
--- install.sh
+++ install.sh
@@ -12,10 +12,16 @@ sqitch config --user engine.pg.client /usr/bin/psql
 #start postgresql daemon
 sudo /sbin/chkconfig --levels 235 postgresql on
 sudo service postgresql start
-echo 'Database server started.'
+echo 'Database server started....'
 
 #Create database
 #cd ~/customers
 #sudo cat customer.sql | mysql --user=root --password=''
+
+echo 'Setting permission....'
+cd /home/
+sudo chmod -R 755 /home/ec2-user/
+
+
 sudo -u postgres -H sh -c 'createdb nodejs; cd ~/customers/sqls/bundle; sqitch deploy db:pg:nodejs'
-echo 'Database created.'
+echo 'Database created....'
