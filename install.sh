c825a5374230c779c9b196a8ca44d46505693ad7
vagrant <vagrant@vagrant.vm>
2018-04-21 10:48:08 +0000

    title changed.

--- install.sh
+++ install.sh
@@ -4,23 +4,12 @@ sudo set -e
 # update instance
 sudo yum -y update
 
-#install sqitch
-curl -X GET -o RPM-GPG-KEY-lambda-epll https://lambda-linux.io/RPM-GPG-KEY-lambda-epll
-sudo rpm --import RPM-GPG-KEY-lambda-epll
-curl -X GET -o epll-release-2017.03-1.2.ll1.noarch.rpm https://lambda-linux.io/epll-release-2017.03-1.2.ll1.noarch.rpm
-sudo yum -y install epll-release-2017.03-1.2.ll1.noarch.rpm
-sudo yum --enablerepo=epll install sqitch-pg94
 
 #set psql path
 sqitch config --user engine.pg.client /usr/bin/psql
 
 #install postgresql
-#sudo yum install mysql-server -y
-sudo yum -y install postgresql postgresql-server postgresql-devel postgresql-contrib postgresql-docs
-sudo service postgresql initdb
-
 #start postgresql daemon
-#sudo service mysqld start
 sudo /sbin/chkconfig --levels 235 postgresql on
 sudo service postgresql start
 
