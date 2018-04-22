1f74af113165b78da28c3caaace07d581b5016a1
PostgreSQL administrator <postgres@vagrant.vm>
2018-04-22 16:38:17 +0000

    install shell updated.

--- install.sh
+++ install.sh
@@ -16,12 +16,13 @@ echo 'Database server started....'
 
 echo 'Setting permission....'
 cd /home/
-sudo chmod -R 755 /home/ec2-user/customers/
+sudo chmod -R 755 /home/ec2-user
 sudo cp /home/ec2-user/customers/pg_hba.conf /var/lib/pgsql9/data/pg_hba.conf
 
 sudo service postgresql restart
 echo 'Database server re-started....'
 
 psql -U postgres -c "ALTER USER postgres WITH PASSWORD '123456'"
-sudo -u postgres -H sh -c 'cd /home/ec2-user/customers/sql/; createdb nodejs; sqitch --engine pg deploy db:pg:nodejs'
+
+sudo -u postgres -H sh -c 'createdb nodejs; cd /home/ec2-user/customers/sql/; sqitch --engine pg deploy db:pg:nodejs'
 echo 'Database deployed....'
