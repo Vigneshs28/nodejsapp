41513645bcbcc35c09ac1e6a1f04e494b8b5666f
tetradev01 <tetradev01@tetratutorials.com>
2018-05-12 15:36:43 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var request = require('request');
 
 it('Main page content', function(done) {
     request('http://10.12.108.11:1337/' , function(error, response, body) {
-        expect(body).to.equal('Hello World');
+        expect(body).to.equal('Hello Tetra');
         done();
     });
 });
