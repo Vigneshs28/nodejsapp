526459728dac6eb83c26822dc8c64b267fe14e9c
tetradev01 <tetradev01@tetratutorials.com>
2018-05-12 15:35:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app=http.createServer(function (req, res) {
 //creates server
   res.writeHead(200, {'Content-Type': 'text/plain'});
   //sets the right header and status code
-  res.end('Hello World');
+  res.end('Hello Tetra');
   //outputs string with line end symbol
 }).listen(1337, "10.12.108.11");
 //sets port and IP address of the server
