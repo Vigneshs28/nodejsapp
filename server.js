a65aa48e8b131a4c89cafc53c8b22b1439691f88
Vigneshs28 <43373272+Vigneshs28@users.noreply.github.com>
2020-04-02 23:26:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app=http.createServer(function (req, res) {
 //creates server
   res.writeHead(200, {'Content-Type': 'text/html'});
   //sets the right header and status code
-  res.end('<h2>Welcome to Vignesh NodeJS Application</h2><h3>Cheers!!! Node.js Deployed on Local Server</h3>');
+  res.end('<h2 align="center">Welcome to Vignesh NodeJS Application</h2><p align="center">Cheers!!! Node.js Deployed on Local Server</p>');
   //outputs string with line end symbol
 }).listen(8081);
 //sets port and IP address of the server
