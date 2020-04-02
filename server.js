13deaefb318b8e663e674e76ef50f6e84d0aedb6
Vigneshs28 <43373272+Vigneshs28@users.noreply.github.com>
2020-04-02 23:11:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var app=http.createServer(function (req, res) {
 //creates server
   res.writeHead(200, {'Content-Type': 'text/plain'});
   //sets the right header and status code
-  res.end('Welcome to TetraNoodle tutorials');
+  res.end('Welcome to Vignesh NodeJS Application');
   //outputs string with line end symbol
-}).listen(1337);
+}).listen(8081);
 //sets port and IP address of the server
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:8081/');
